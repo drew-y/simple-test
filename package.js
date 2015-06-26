ba80af147563877cf9e70076ec79552a030c07dd
Drew-Y <drew.youngwerth@gmail.com>
2015-06-26 13:37:04 -0600

    update package.js

--- package.js
+++ package.js
@@ -1,22 +1,27 @@
 Package.describe({
   name: 'drewy:simple-test',
-  version: '0.0.1',
+  version: '0.1.0',
+
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Dead simple unit tests that aid in programming',
+
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/drew-y/simple-test',
+
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('simple-test.js');
+  api.use(['ddp', 'grigio:babel']);
+  api.addFiles('simple-test.es6.js');
+  api.export(['tests', 'SimpleTest']);
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
+  api.use(['tinytest', 'test-helpers']);
   api.use('drewy:simple-test');
   api.addFiles('simple-test-tests.js');
 });
